5cb6ec5394aaef7fad877f680dd5209a2570159f
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-05 04:36:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     array.push("I am strange loop");
-    if (i == 1) {
+    /*if (i == 1) {
       array.push("I am 1 strange loop.");
     } else {
       array.push("I am " + i + "strange loops.");
-    }
+    }*/
   }
 }
 
